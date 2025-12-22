c1e223cd754a18a67d572d8734777a671f1fa538
SUPHOT NARAPONG <suphot.n@gmail.com>
2025-12-22 22:53:48 +0700

    Make Manage Expense to got button base on update or add new

--- app/(screens)/ManageExpense.tsx
+++ app/(screens)/ManageExpense.tsx
@@ -1,6 +1,10 @@
+import { StyleSheet, View } from 'react-native';
+
+import Button from '@/components/ui/Button';
+import IconButton from '@/components/ui/IconButton';
+import { GlobalStyles } from '@/constants/styles';
 import { useLocalSearchParams, useNavigation } from 'expo-router';
 import { useLayoutEffect } from 'react';
-import { Text, View } from 'react-native';
 
 type ManageExpenseParams = {
   expenseId?: string;
@@ -18,9 +22,63 @@ export default function ManageExpense() {
     });
   }, [navigation, isEditing]);
 
+  function deleteExpenseHandler() {
+    navigation.goBack();
+  }
+
+  function cancelHandler() {
+    navigation.goBack();
+  }
+
+  function confirmHandler() {
+    navigation.goBack();
+  }
+
   return (
-    <View>
-      <Text>{isEditing ? 'Editing' : 'Adding'} Expense</Text>
+    <View style={styles.container}>
+      <View style={styles.buttons}>
+        <Button style={styles.button} mode="flat" onPress={cancelHandler}>
+          Cancel
+        </Button>
+        <Button style={styles.button} onPress={confirmHandler}>
+          {isEditing ? 'Update' : 'Add'}
+        </Button>
+      </View>
+      {isEditing && (
+        <View style={styles.deleteContainer}>
+          <IconButton
+            icon="trash"
+            color={GlobalStyles.colors.error500}
+            size={36}
+            onPress={deleteExpenseHandler}
+          />
+        </View>
+      )}
     </View>
   );
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: GlobalStyles.colors.primary800,
+  },
+  buttons: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  button: {
+    minWidth: 120,
+    marginHorizontal: 8,
+  },
+  deleteContainer: {
+    marginTop: 16,
+    paddingTop: 8,
+    borderTopWidth: 2,
+    borderTopColor: GlobalStyles.colors.primary200,
+    alignItems: 'center',
+  },
+});
