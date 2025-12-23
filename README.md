a7a139cca275fe2128fb049044a63af209a851f6
SUPHOT NARAPONG <suphot.n@gmail.com>
2025-12-23 15:40:20 +0700

    Transforming & Using Fetched Data

--- README.md
+++ README.md
@@ -26,6 +26,9 @@
    5.2 + item and it will be record in firebase
    5.3 Fetching Backend Data (GET Requests) Result 
     LOG  {"-Oh9CZGcK4xAX9i_bCrE": {"amount": 12, "date": "2025-12-23T00:00:00.000Z", "description": "My Request"}, "-Oh9D8r5KDroX7QaEMZl": {"amount": 13, "date": "2025-12-20T00:00:00.000Z", "description": "2nd timeSending POST Http Requests"}}
+   
+   5.4 const [fatchedExpense, setFatchedExpense] = useState<Expense[]>([]);
+      It Record to db but no update in screen 
 
 
 
--- app/(tabs)/RecentExpenses.tsx
+++ app/(tabs)/RecentExpenses.tsx
@@ -1,25 +1,27 @@
 // app/(tabs)/RecentExpenses.tsx
 import ExpensesOutput from '@/components/ExpensesOutput/ExpensesOutput';
-import { ExpensesContext } from '@/store/expenses-context';
-import { useContext, useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
+import { Expense } from '@/models/expense';
 import { fetchExpenses } from '@/utils/http';
 
 const getDateMinusDays = (date: Date, days: number) =>
   new Date(date.getFullYear(), date.getMonth(), date.getDate() - days);
 
 export default function RecentExpenses() {
-  const expensesCtx = useContext(ExpensesContext);
+  // const expensesCtx = useContext(ExpensesContext);
+  const [fatchedExpense, setFatchedExpense] = useState<Expense[]>([]);
 
   useEffect(() => {
     async function getExpenses() {
-      await fetchExpenses()    
+      const expenses = await fetchExpenses();
+      setFatchedExpense(expenses)    
     }
 
     getExpenses();
   },[])
 
-  const recentExpenses = expensesCtx.expenses.filter((expense) => {
+  const recentExpenses = fatchedExpense.filter((expense) => {
     const today = new Date();
     const date7DaysAgo = getDateMinusDays(today, 7);
     return expense.date >= date7DaysAgo;
